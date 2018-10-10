e0b1faeb1457ec461515626ff360b4632c8fac31
Carsten König <carsten@gettingsharper.de>
2018-10-10 12:57:39 +0200

    EventStore und Konto auf Templates zurückgesetzt

--- Examples/EventStore/src/EventStore.hs
+++ Examples/EventStore/src/EventStore.hs
@@ -5,18 +5,11 @@ module EventStore
   , Sql.aggregateIds
   , Sql.startAggregate
   , Sql.insertEvent
-  , Projection
-  , load
+  , Sql.foldEvents
   ) where
 
 import           Control.Monad.IO.Class (MonadIO)
 import           Data.Aeson (FromJSON)
 import           EventStore.Event
-import           EventStore.Projections (Projection)
-import qualified EventStore.Projections as Projections
 import           EventStore.Sqlite (Handle)
 import qualified EventStore.Sqlite as Sql
-
-
-load :: MonadIO m => FromJSON ev => Handle -> Projection ev res -> AggregateId -> m res
-load handle proj aId = Projections.use (Sql.foldEvents handle aId) proj
--- Examples/Konto/src/Konto.hs
+++ Examples/Konto/src/Konto.hs
@@ -16,61 +16,30 @@ module Konto
 import           Data.Aeson (FromJSON, ToJSON)
 import           Data.Decimal (Decimal, DecimalRaw(..))
 import qualified EventStore as Store
-import qualified EventStore.Projections as Projections
 import           GHC.Generics
 
 
 data Konto
-  = Konto Store.Handle Store.AggregateId
+  = Konto
 
 
 initialisieren :: FilePath -> IO Konto
-initialisieren dbFile = do
-  handle <- Store.initHandle dbFile
-  agIds <- Store.aggregateIds handle
-  case agIds of
-    (kontoId:_) -> return $ Konto handle kontoId
-    [] -> do
-      kontoId <- Store.startAggregate handle (Haben "Konto eröffnet" 0)
-      return $ Konto handle kontoId
+initialisieren dbFile = undefined
 
 
 einzahlen :: Konto -> String -> Decimal -> IO Decimal
-einzahlen konto@(Konto handle kontoId) text betrag = do
-  Store.insertEvent handle kontoId (Haben text betrag)
-  stand konto
+einzahlen = undefined
 
 
 abheben :: Konto -> String -> Decimal -> IO Decimal
-abheben konto@(Konto handle kontoId) text betrag = do
-  Store.insertEvent handle kontoId (Soll text betrag)
-  stand konto
+abheben = undefined
 
 
 stand :: Konto -> IO Decimal
-stand (Konto handle kontoId) =
-  Store.load handle kontostandP kontoId
-
-
-data Buchung
-  = Haben String Decimal
-  | Soll  String Decimal
-  deriving (Generic, Show)
-
-
-instance FromJSON Buchung
-instance ToJSON Buchung
+stand = undefined
 
 
 -- brauchen leider ein paar Orphans
 deriving instance Generic (DecimalRaw Integer)
 instance FromJSON Decimal
 instance ToJSON Decimal
-
-
--- Projektionen
-kontostandP :: Store.Projection Buchung Decimal
-kontostandP = Projections.sum
-              (\case
-                  (Haben _ p) -> p
-                  (Soll  _ n) -> negate n)
