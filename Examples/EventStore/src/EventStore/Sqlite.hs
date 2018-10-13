5280eff2e164dd8d399fd4bb0ede55eed2650ed3
Carsten König <carsten@gettingsharper.de>
2018-10-13 08:10:03 +0200

    Revert "EventStore und Konto auf Templates zurückgesetzt"
    This reverts commit e0b1faeb1457ec461515626ff360b4632c8fac31.

--- Examples/EventStore/src/EventStore/Sqlite.hs
+++ Examples/EventStore/src/EventStore/Sqlite.hs
@@ -18,7 +18,7 @@ import qualified Data.Aeson as Aeson
 import           Data.Maybe (mapMaybe)
 import qualified Data.UUID as UUID
 import qualified Data.UUID.V4 as UUID
-import           Database.SQLite.Simple (Connection, Query)
+import           Database.SQLite.Simple (Connection, Query, (:.)(..))
 import qualified Database.SQLite.Simple as Sql
 import           EventStore.Event (Event(Event), AggregateId, Number)
 import qualified EventStore.Event as Event
@@ -27,28 +27,56 @@ import qualified EventStore.Event as Event
 data Handle = Handle { connectionString :: String }
 
 initHandle :: MonadIO m => String -> m Handle
-initHandle conStr = undefined
+initHandle conStr = do
+  let handle = Handle conStr
+  initializeDb handle
+  return handle
+  where
+    initializeDb handle = withHandle handle $ \conn ->
+      Sql.execute_ conn "CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY, aggregateId TEXT, json TEXT)"
 
 
 aggregateIds :: MonadIO m => Handle -> m [AggregateId]
-aggregateIds handle = undefined
+aggregateIds handle = withHandle handle $ \ conn -> do
+  aggIds <- Sql.query_ conn "SELECT DISTINCT aggregateId FROM events"
+  return $ mapMaybe (UUID.fromString . Sql.fromOnly) aggIds
 
 
 startAggregate :: MonadIO m => ToJSON ev => Handle -> ev -> m AggregateId
-startAggregate handle event = undefined
+startAggregate handle event = withHandle handle $ \ conn -> do
+  aId <- UUID.nextRandom
+  Sql.execute conn "INSERT INTO events (aggregateId, json) VALUES (?,?)" (UUID.toString aId, Aeson.encode event)
+  return aId
 
 
 insertEvent :: MonadIO m => ToJSON ev => Handle -> AggregateId -> ev -> m Number
-insertEvent handle aId event = undefined
+insertEvent handle aId event = withHandle handle $ \ conn -> do
+  Sql.execute conn "INSERT INTO events (aggregateId, json) VALUES (?,?)" (UUID.toString aId, Aeson.encode event)
+  Sql.lastInsertRowId conn
 
 
 allEvents :: MonadIO m => FromJSON ev => Handle -> m [Event ev]
-allEvents handle = undefined
+allEvents handle = reverse <$> foldAllEvents handle (flip (:)) []
 
 
 foldEvents :: MonadIO m => FromJSON ev => Handle -> AggregateId -> (s -> ev -> s) -> s -> m s
-foldEvents handle aId fold s0 = undefined
+foldEvents handle aId fold s0 = withHandle handle $ \ conn ->
+  Sql.fold conn "SELECT * FROM events WHERE aggregateId = ?" (Sql.Only (UUID.toString aId)) s0 foldQuery
+  where foldQuery s (Event _ _ ev) = return $ fold s ev
 
 
 foldAllEvents :: MonadIO m => FromJSON ev => Handle -> (s -> Event ev -> s) -> s -> m s
-foldAllEvents handle fold s0 = undefined
+foldAllEvents handle fold s0 = withHandle handle $ \ conn ->
+  Sql.fold_ conn "SELECT * FROM events" s0 foldQuery
+  where foldQuery s ev = return $ fold s ev
+
+
+withHandle :: MonadIO m => Handle -> (Connection ->  IO a) -> m a
+withHandle (Handle conStr) action = liftIO $ Sql.withConnection conStr action
+
+
+instance FromJSON a => Sql.FromRow (Event a) where
+  fromRow = Event <$> Sql.field <*> uuidField <*> jsonField
+    where
+      uuidField = Sql.field >>= maybe (fail "could not parse UUID") return . UUID.fromString
+      jsonField = Sql.field >>= maybe (fail "cound not decode JSON") return . Aeson.decode
--- Examples/Konto/src/Konto.hs
+++ Examples/Konto/src/Konto.hs
@@ -16,30 +16,61 @@ module Konto
 import           Data.Aeson (FromJSON, ToJSON)
 import           Data.Decimal (Decimal, DecimalRaw(..))
 import qualified EventStore as Store
+import qualified EventStore.Projections as Projections
 import           GHC.Generics
 
 
 data Konto
-  = Konto
+  = Konto Store.Handle Store.AggregateId
 
 
 initialisieren :: FilePath -> IO Konto
-initialisieren dbFile = undefined
+initialisieren dbFile = do
+  handle <- Store.initHandle dbFile
+  agIds <- Store.aggregateIds handle
+  case agIds of
+    (kontoId:_) -> return $ Konto handle kontoId
+    [] -> do
+      kontoId <- Store.startAggregate handle (Haben "Konto eröffnet" 0)
+      return $ Konto handle kontoId
 
 
 einzahlen :: Konto -> String -> Decimal -> IO Decimal
-einzahlen = undefined
+einzahlen konto@(Konto handle kontoId) text betrag = do
+  Store.insertEvent handle kontoId (Haben text betrag)
+  stand konto
 
 
 abheben :: Konto -> String -> Decimal -> IO Decimal
-abheben = undefined
+abheben konto@(Konto handle kontoId) text betrag = do
+  Store.insertEvent handle kontoId (Soll text betrag)
+  stand konto
 
 
 stand :: Konto -> IO Decimal
-stand = undefined
+stand (Konto handle kontoId) =
+  Store.load handle kontostandP kontoId
+
+
+data Buchung
+  = Haben String Decimal
+  | Soll  String Decimal
+  deriving (Generic, Show)
+
+
+instance FromJSON Buchung
+instance ToJSON Buchung
 
 
 -- brauchen leider ein paar Orphans
 deriving instance Generic (DecimalRaw Integer)
 instance FromJSON Decimal
 instance ToJSON Decimal
+
+
+-- Projektionen
+kontostandP :: Store.Projection Buchung Decimal
+kontostandP = Projections.sum
+              (\case
+                  (Haben _ p) -> p
+                  (Soll  _ n) -> negate n)
